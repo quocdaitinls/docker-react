6373abbd9ef020cccfcd41d322d2bf6436c683d9
Quoc Dai <quocdaitinls@gmail.com>
2022-05-24 15:48:16 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          xin chao cac ban !!
+          I was changed on the feature branch !!!
         </a>
       </header>
     </div>
